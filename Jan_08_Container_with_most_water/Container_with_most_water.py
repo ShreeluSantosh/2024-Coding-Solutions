15fa6fc64e03ea982efee42b6551033c563665e4
Shreelu Santosh <94289402+ShreeluSantosh@users.noreply.github.com>
2024-01-08 15:13:55 +0530

    Update Container_with_most_water.py

--- Jan_08_Container_with_most_water/Container_with_most_water.py
+++ Jan_08_Container_with_most_water/Container_with_most_water.py
@@ -1,5 +1,16 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        m = nums[::-1]
-        return m[k-1]
+    def maxArea(self, height: List[int]) -> int:
+        mx = 0
+        i = 0
+        j  = len(height)-1
+        while i < j:
+            indDiff = j-i
+            m = min(height[i], height[j])
+            area = indDiff*m
+            if area > mx:
+                mx = area
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return mx
